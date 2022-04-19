d481a4f676279421a8e1dc95fe198c6d755f9166
Peter Law <PeterJCLaw@gmail.com>
2022-04-19 16:07:29 +0100

    Modernise swap script

--- checks/swap.py
+++ checks/swap.py
@@ -1,33 +1,41 @@
-import sys
+import argparse
+from pathlib import Path
 
-if len(sys.argv) != 2 or '--help' in sys.argv:
-    print('Usage: swap.py <schedule-file> <find-tla> <replace-tla>')
-    print('  Searches for a match that contains the first team, but not the second')
-    print('  and replaces the first for the second in that match.')
-    exit(1)
+import helpers
 
-f = open(sys.argv[1])
-lines = f.read().split("\n")
-f.close()
 
-with open(sys.argv[1], 'w+') as f:
+def main(schedule_file: Path, find: str, replace: str) -> None:
+    lines = schedule_file.read_text().splitlines(keepends=False)
 
-    for i in range(len(lines)):
-        line = lines[i]
-        parts = line.strip().split('|')
+    find = find.upper()
+    replace = replace.upper()
 
-        find = sys.argv[2].upper()
-        repl = sys.argv[3].upper()
-        if find in parts and repl not in parts:
+    for i, line in enumerate(lines):
+        parts = line.strip().split(helpers.SEPARATOR)
+
+        if find in parts and replace not in parts:
             print(parts)
             idx = parts.index(find)
-            parts[idx] = repl
+            parts[idx] = replace
             print(parts)
 
-            line = '|'.join(parts)
+            line = helpers.SEPARATOR.join(parts)
             lines[i] = line
             break
 
-    text = "\n".join(lines)
-    f.seek(0)
-    f.write(text)
+    schedule_file.write_text('\n'.join(lines))
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description=(
+        "Searches for a match that contains the first entrant, but not the second"
+        "and replaces the first for the second in that match."
+    ))
+    parser.add_argument('schedule_file', type=Path, help="File to search and modify")
+    parser.add_argument('find', help="Entrant to search for")
+    parser.add_argument('replace', help="Entrant to replace with")
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    main(**parse_args().__dict__)
