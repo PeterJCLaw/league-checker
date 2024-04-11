140f7a0351a639a1669e8adf523a7a9ee14e2aa9
Peter Law <PeterJCLaw@gmail.com>
2024-04-11 16:14:49 +0100

    Sort closeness by count of min break gap, not TLA

--- checks/close.py
+++ checks/close.py
@@ -24,9 +24,9 @@ def pairwise(iterable: Iterable[T]) -> Iterable[tuple[T, T]]:
     return zip(a, b)
 
 
-def _sort_key(value: tuple[str, int, list[int]]) -> tuple[int, helpers.HumanSortTuple]:
-    tla, min_break, _ = value
-    return min_break, helpers.human_sort_key(tla)
+def _sort_key(value: tuple[str, int, list[int], collections.Counter[int]]) -> tuple[int, helpers.HumanSortTuple]:
+    tla, min_break, _, counts = value
+    return min_break, -counts[min_break], helpers.human_sort_key(tla)
 
 
 def main(schedule_file: Path) -> None:
@@ -51,16 +51,21 @@ def main(schedule_file: Path) -> None:
             diff = match - last_match
             breaks[tla].append(diff)
             last_match = match
-        min_breaks.append((tla, min(breaks[tla]), breaks[tla]))
+        tla_breaks = breaks[tla]
+        min_breaks.append((
+            tla,
+            min(tla_breaks),
+            tla_breaks,
+            collections.Counter(tla_breaks),
+        ))
 
     print('Team\tMin-gap\tCount\tGaps')
 
     count_n = 0
-    for tla, min_break, btla in sorted(min_breaks, key=_sort_key):
-        c = collections.Counter(btla)
+    for tla, min_break, tla_breaks, counts in sorted(min_breaks, key=_sort_key):
         if min_break == WARN_MIN_GAP:
             count_n += 1
-        print(f"{tla}\t{min_break}\t{c[min_break]}\t{btla}")
+        print(f"{tla}\t{min_break}\t{counts[min_break]}\t{tla_breaks}")
 
     print()
     print(f"{count_n} teams have a minimum gap of {WARN_MIN_GAP}")
